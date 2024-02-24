12b6673ecd133980efb61488453ca72b7e8dde31
Washington Rodrigues <washingtonjunio16@gmail.com>
2024-02-24 16:04:00 -0300

    chore: create table customers

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 0) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_24_182749) do
   # These are extensions that must be enabled in order to support this database
+  enable_extension "pgcrypto"
   enable_extension "plpgsql"
+  enable_extension "uuid-ossp"
+
+  create_table "customers", id: :uuid, default: -> { "uuid_generate_v4()" }, force: :cascade do |t|
+    t.string "name", limit: 250, null: false
+    t.string "document", limit: 14, null: false
+    t.string "state", limit: 100, null: false
+    t.string "city", limit: 100, null: false
+    t.string "zipcode", limit: 12, null: false
+    t.string "address", limit: 250, null: false
+    t.string "neighborhood", limit: 100, null: false
+    t.datetime "deleted_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["document", "deleted_at"], name: "index_customers_on_document_and_deleted_at", unique: true
+  end
 
 end
