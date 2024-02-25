ade0981b44d6f33608f3bb0442e4cf4893e329e2
Washington Rodrigues <washingtonjunio16@gmail.com>
2024-02-25 17:10:40 -0300

    fix: spec Bankslip::Record

--- app/models/bankslip/record.rb
+++ app/models/bankslip/record.rb
@@ -7,7 +7,7 @@ module Bankslip
     enum :status, %w[opened overdue canceled expired].index_by(&:itself), default: 'opened'
     enum :gateway, %w[kobana].index_by(&:itself), default: 'kobana'
 
-    validates :expire_at, comparison: { greater_than: DateTime.current }
+    validates :expire_at, comparison: { greater_than: DateTime.now.end_of_day }
     validates :amount, comparison: { greater_than: 0 }
 
     belongs_to :customer, class_name: 'Customer::Record'
--- spec/models/bankslip/record_spec.rb
+++ spec/models/bankslip/record_spec.rb
@@ -10,9 +10,9 @@ RSpec.describe Bankslip::Record, :model do
         .backed_by_column_of_type(:string)
     }
 
-    it { should define_enum_for(:gateway).with_values({ kobana: 'kobana' }).backed_by_column_of_type(:string) }
-    it { should validate_comparison_of(:expire_at).is_greater_than(DateTime.current) }
-    it { should validate_comparison_of(:amount).is_greater_than(0) }
     it { should belong_to(:customer).class_name('Customer::Record') }
+    it { should define_enum_for(:gateway).with_values({ kobana: 'kobana' }).backed_by_column_of_type(:string) }
+    it { should validate_comparison_of(:amount).is_greater_than(0) }
+    it { should validate_comparison_of(:expire_at).is_greater_than(DateTime.now.end_of_day) }
   end
 end
